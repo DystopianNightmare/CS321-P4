c3f068a63ec13b776f2e6bb36e1082996f70afb9
joemgreen22 <52195596+joemgreen22@users.noreply.github.com>
2020-04-26 04:59:42 -0600

    Update BTree.java

--- BTree.java
+++ BTree.java
@@ -1,140 +1,189 @@
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStream;
 
-/**
- * Main driver file to read file and parse data
- * @author josh
- *
- */
-public class GeneBankCreateBTree {
+public class BTree {
 
-	private static Integer t; 		//degree to be used -- this is the value for degree
-	//	private static BufferedReader br;
-	private static int cacheSize;		//if cache is used this will be the cache size
-	private static int debugLevel;		//to be used later
-	private static boolean useCache = false; //false if arg[0] is 0
-	private static int sequenceLength;		//this is the k value
-	private static  String nameOfTree; // this will be the name of the binary file
-	private BTree tree;
+	private BTreeNode root, curr, parent, child, node;
+	private int m;
+	private File file;
+	private int t;
+	private int middle; 			// is the middle node
 
-	public static void main(String[] args) throws IOException {
+	public BTree(String name, int t)  {
+		//make a new file to write to. M is number of values per node, call btreecreate to get a tree with 1 node
+		this.t = t;
+		m= t*2;
+		middle = (int)Math.floor(t);	//find middle node
 
-		//use a try-catch to parse all arguments. incorrect param will print usage
-		try {
-			//use caching or not
-			if(Integer.parseInt(args[0]) == 1) {
-				useCache = true;
-				cacheSize = Integer.parseInt(args[4]);
-				if(args[5] != null) {								//if cache is being used a cache, debug is in position 5 of args
-					debugLevel = Integer.parseInt(args[5]);
-				}
-			}else {
-				if(args.length == 5) {								////if cache is being used a cache, debug is in position 4 of array since cache size is not used
-					debugLevel = Integer.parseInt(args[4]);
-				}
+		root = BTreeCreateNode();
+	}
+
+	public BTreeNode BTreeCreateNode()  {
+		node = new BTreeNode(m);
+
+		return node;
+	}
+	
+	public boolean checkChildIsFull(BTreeNode node, long val) {
+		int i = 0;
+		while(i < node.getCurrentlyStored() && val > node.getBTreeObject(i).getKey()) {
+			i++;
+		}
+		if (node.getIsLeaf()== true) {
+			return true;
+		}
+		BTreeNode childNode = node.getChildNode(i);
+		if (childNode.getCurrentlyStored() == m-1 && childNode.getIsLeaf()) {
+			return true;
+		}
+		else if (childNode.getCurrentlyStored() == m-1 && !childNode.getIsLeaf()) {
+			boolean temp = checkChildIsFull(childNode, val);
+			return temp;
 			}
+		return false;
+	}
 
-			//parse degree
-			t = Integer.parseInt(args[1]);	
-			if(t == 0) {
-				//implement something to use the optimal degree
-			}
-			//get sequence length
-			sequenceLength = Integer.parseInt(args[3]);
-			if(sequenceLength < 0 || sequenceLength > 31) {
-				throw new Exception();
-			}
-			//parse debug level
-		}catch(Exception e) {
-			printUsage();
+	
+	public void BTreeInsert(long val)  {																					// INSERT
+
+		node = root;
+		if(node.getCurrentlyStored() == m-1 && checkChildIsFull(node, val)) {
+			BTreeNode s = new BTreeNode(m);
+			root = s;
+			s.setIsLeaf(false);
+			s.setCurrentlyStored(0);
+			s.setChildPointer(node, 0);
+
+			s.setIsRoot(true);
+
+
+			insertNodeNonFull(node,val);
+			BTreeSplitChild(s,val);
+
+			//			insertNodeNonFull(s,val);
+		}else {
+			insertNodeNonFull(root, val);	
+		}
+	}
+	public void BTreeSplitChild(BTreeNode parent, long zz) {																		// SPLIT CHILD
+
+		//*** changed for loops to start at zero since arrays being used start at 0
+
+		BTreeNode z = new BTreeNode(m);		//z = new right child
+		BTreeNode y = parent.getChildNode(parent.getCurrentlyStored());	//y = old root
+		z.setIsLeaf(y.getIsLeaf());
+
+		y.setParentPointer(parent);
+		z.setParentPointer(parent);
+
+		parent.setBTreeObject(y.getBTreeObject(middle-1), parent.getCurrentlyStored());
+		parent.incrementCurrentlyStored();
+		
+		parent.setChildPointer(z, parent.getCurrentlyStored());
+
+
+		//in psuedocode we set z's n to t-l, but i ddont know if we need to if we call that value from Tree and set globally
+		// take top half objects from array y and put in array z
+		for(int j =0; j<middle; j++) {
+			z.setBTreeObject(y.getBTreeObject(j+middle), j); //z.key = y.key+t
 		}
 
-		//get gbk file
-		String fileName = args[2];
-		
-		//Get name of Binary file and make a new BTree
-		
-		nameOfTree = (fileName + ".btree.data." + sequenceLength + "." + t );	//This is the name of the binary file
-		BTree tree = new BTree(nameOfTree,t);
-		
-		try {
-			Scanner scan = new Scanner(new FileReader(fileName));
-			String line;
+		if(!y.getIsLeaf()) {	//if y is not a leaf
+			for(int j = 0; j < middle+1;j++) {
+				z.setChildPointer(y.getChildNode(j+middle), j); 		//sets z's child pointers to that of y+t
+			}
+		}
 
-			while(scan.hasNextLine()) {
-				line = scan.nextLine();
+		y.setCurrentlyStored(middle-1);
+		z.setCurrentlyStored(middle);			// -1 ????
+
+//		for(int j = parent.getCurrentlyStored(); j > middle+1; j--) {		//what is i?
+//			parent.setChildPointer(parent.getChildNode(j), j-1);
+//		}
+
+		parent.setChildPointer(z, parent.getCurrentlyStored());
+
+
+	}
+
+	public void insertNodeNonFull(BTreeNode node, long val) {	
+		int i = node.getCurrentlyStored();
+
+		// if node is a leaf
+		if(node.getIsLeaf()) {
+
+			while(i >= 1 && val < node.getBTreeObject(i-1).getKey()) {				//this line and next i had to change i to i-1 -- probaly going to have to for remainder as well
+				node.setBTreeObject(node.getBTreeObject(i-1), i);
+				i--;
+			}
+			
+			BTreeObject obj = new BTreeObject(val);
+			node.setBTreeObject(obj, i);
+			node.incrementCurrentlyStored();	
+		}
+		
+		// if node is not a leaf
+		else{
+			 i = 0;
+			while(i < node.getCurrentlyStored() && val > node.getBTreeObject(i).getKey()) {
+				i++;
+			}
+			
+			BTreeNode childNode = node.getChildNode(i);								/// 	THIS FUCKING BREAKS IT!!!!!!!!!!!!!!!!!!11
+
+			if(childNode.getCurrentlyStored() ==  m-1 && checkChildIsFull(childNode, val)) {
 				
-				while(line.startsWith("ORIGIN")) {
-					String character = "";
-					line =  scan.nextLine();
-					
-					while(!line.startsWith("//")) {
-						for(int i = 0; i < line.length(); i++) {
-							if(line.charAt(i) == 'a' || line.charAt(i) == 'c' || line.charAt(i) == 'g' || line.charAt(i) == 't') {
-								character += line.charAt(i);
-							}
-						}
-						line = scan.nextLine();
-					}
-
-					for(int i = 0; i < character.length()-sequenceLength+1;i++) {
-						String s = "";
-						for(int j = 0; j < sequenceLength; j++) {
-							if(character.charAt(i+j) == 'a' || character.charAt(i+j) == 'A') {
-								s += "00";
-							}
-							if(character.charAt(i+j) == 't' || character.charAt(i+j) == 'T') {
-								s += "11";
-							}
-							if(character.charAt(i+j) == 'c' || character.charAt(i+j) == 'C') {
-								s += "01";
-							}
-							if(character.charAt(i+j) == 'g' || character.charAt(i+j) == 'G') {
-								s += "10";
-							}
-							if(character.charAt(i+j) == 'n' || character.charAt(i+j) == 'N') {
-								s += "n";
-							}
-							
-						}
-						//OKAY! We now have s which we will use
-						// S is the String of 0 ad 1's to converted to long - 0's in the front are dropped until the first 1, so 00000111 is just 111
-						
-						if(!s.contains("n")) {
-						Long key = Long.parseLong(s);
-						}
-//						tree.addObjectToNode(key);
-
-						
-					}
-
-				}
-
+				
+				insertNodeNonFull(childNode,val);
+				BTreeSplitChild(node,val);
+				
+				
+				
+//				BTreeNode parentNode = childNode.getParentPointer();
+//				parentNode.setIsLeaf(false);
+//			
+//				parentNode.setChildPointer(childNode, parentNode.getCurrentlyStored());
+//				parentNode.setBTreeObject(childNode.getBTreeObject(middle), parentNode.getCurrentlyStored());
+//				childNode.setParentPointer(parentNode);
+//
+//				BTreeSplitChild(parentNode, val);
+//				i = parentNode.getCurrentlyStored();
+//				
+//				childNode = parentNode;
 			}
-			scan.close();
-			
-		} catch ( Exception e) {
-			e.printStackTrace();
-		}
-		//TESTING 1 INPUT
-		Long key = (long) 33;
-		
-//	the folllowing is to add XX number of values to tree and then print the tree
-		for(int i =0; i< 18; i++) {
-			long keytest = (long) i;
-			System.out.println(keytest);
-			if(keytest == 13) {
-//				tree.printTree();
-				System.out.println("testing");
+			else {
+				insertNodeNonFull(childNode, val);
 			}
-			tree.BTreeInsert(keytest);
-		}
-		tree.printTree();
 			
-		
+		}
 	}
-	public static void printUsage() {
-		System.out.println("java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
+
+	public void printTree() {
+		System.out.println(" ");	//blank space
+		root.traverse();
+		print();
 	}
+	   public void print () {
+		    // Print a textual representation of this B-tree.
+		        printSubtree(root, "");
+		    }
+
+		    private static void printSubtree (BTreeNode top, String indent) {
+		    // Print a textual representation of the subtree of this B-tree whose
+		    // topmost node is top, indented by the string of spaces indent.
+		        if (top == null)
+		            System.out.println(indent + "o");
+		        else {
+		            System.out.println(indent + "o-->");
+		            boolean isLeaf = top.getIsLeaf();
+		            String childIndent = indent + "    ";
+		            for (int i = 0; i < top.getCurrentlyStored(); i++) {
+		                if (! isLeaf)  printSubtree(top.getChildNode(i), childIndent);
+		                System.out.println(childIndent + top.getBTreeObject(i).getKey());
+		            }
+		            if (! isLeaf)  printSubtree(top.getChildNode(top.getCurrentlyStored()), childIndent);
+		        }
+		    }
 }
