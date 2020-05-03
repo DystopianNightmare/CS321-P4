35cc1dd07f3c77999e9153d8b2ee94283e68db1e
DystopianNightmare <52088130+DystopianNightmare@users.noreply.github.com>
2020-05-02 20:58:44 -0600

    Add files via upload

--- GeneBankSearch.java
+++ GeneBankSearch.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GeneBankSearch {
 
@@ -9,12 +11,12 @@ public class GeneBankSearch {
 	private static int cacheSize;		//if cache is used this will be the cache size
 	private static int debugLevel;		//to be used later
 	private static boolean useCache = false; //false if arg[0] is 0
-private static File BTreeFile;
-private static File QueryFile;
-	
-	
+	private static File BTreeFile;
+	private static File QueryFile;
+
+
 	public static void main(String[] args) {
-		
+
 		//use a try-catch to parse all arguments. incorrect param will print usage
 		try {
 			//use caching or not
@@ -32,40 +34,46 @@ private static File QueryFile;
 			//make files from args
 			BTreeFile = new File(args[1]);
 			QueryFile = new File(args[2]);
-		
-			String first = args[1].substring(0, 5);
-			int i = args[1].length();
-			String second = args[1].substring(i-1,i);
+
+			Pattern p = Pattern.compile("-?\\d+");
+			Matcher m = p.matcher(args[1]);
+			String[] arr = new String[5];
+			int j = 0;
+			while(m.find()) {
+			arr[j] = m.group();
+			j++;
+			}
 			
-			PrintWriter pw = new PrintWriter(first +"_query" + second + "_result");
+			PrintWriter pw = new PrintWriter(arr[0] +"_query" + arr[1] + "_result");
 			Scanner scanQ = new Scanner(QueryFile);
 			String lineQ;
 			String lineB;
+			
 			while(scanQ.hasNext()) {
 				Scanner scanB = new Scanner(BTreeFile);
-				 lineB = scanB.nextLine();
+				lineB = scanB.nextLine();
 				lineQ=scanQ.nextLine().toUpperCase();
 				while(scanB.hasNextLine()) {
 					if(lineB.contains(lineQ)) {
 						System.out.println(lineB);
 						pw.println(lineB);
 					}
-					
+
 					lineB=scanB.nextLine();
 				}
 				scanB.close();
 			}
 			scanQ.close();
 			pw.close();
-			
+
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	
-	
-	
+
+
+
 	public static void printUsage() {
 		System.out.println("java GeneBankSearch <0/1(no/with Cache)> <btree file> <query file> [<cache size>] [<debug level>]");
 	}
