9e8cb14b6346cfe4447271f4b8151fbb6136e42b
joemgreen22 <52195596+joemgreen22@users.noreply.github.com>
2020-05-07 04:57:41 -0600

    Update GeneBankSearch.java

--- GeneBankSearch.java
+++ GeneBankSearch.java
@@ -1,15 +1,17 @@
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.RandomAccessFile;
-import java.util.Date;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * This class will search a BTree for values specified in a query file 
+ * @author Josh, Joe, Kate
+ *
+ */
 public class GeneBankSearch {
 
-	private static Integer t; 		//degree to be used -- this is the value for degree
-	//	private static BufferedReader br;
 	private static int cacheSize;		//if cache is used this will be the cache size
 	private static int debugLevel = -1;		//to be used later
 	private static boolean useCache = false; //false if arg[0] is 0
@@ -20,8 +22,8 @@ public class GeneBankSearch {
 	private static long time;
 
 	public static void main(String[] args) {
-		
-		
+
+		time = System.currentTimeMillis();
 		//use a try-catch to parse all arguments. incorrect param will print usage
 		try {
 			//use caching or not
@@ -49,73 +51,50 @@ public class GeneBankSearch {
 				arr[j] = m.group();
 				j++;
 			}
+
 			BTree tree = new BTree();
 
 			RAF = new RandomAccessFile(BTreeFile,"r");
-			File tmpFile = tree.buildTree(RAF);
-
 
 			String pwFileName ="test" +arr[0] +"_query" + arr[1] + "_result";
 			PrintWriter pw = new PrintWriter(pwFileName);
 			Scanner scanQ = new Scanner(QueryFile);
 			String lineQ;
-			String lineTmp;
-			Long tmp;
-
+		
 
 			while(scanQ.hasNext()) {
-				Scanner scanTmp = new Scanner(tmpFile);
 				lineQ=scanQ.nextLine();
 
-				if(useCache) {
-					boolean found = false;
-					while(scanTmp.hasNextLine() && !found) {
-						lineTmp=scanTmp.nextLine();
-						int i = 0;
-						while(lineTmp.charAt(i) != ':') {
-							i++;
-						}
-						String end = (String) lineTmp.substring(i, lineTmp.length());
-						String temp = (String) cache.search(lineTmp.substring(0, i));
 
-						if(temp != null ) {
-							if(temp.equals(lineQ)) {
-								found = true;
-								pw.println(lineQ.toLowerCase() + lineTmp.substring(i, lineTmp.length()));
-								
-							}
-						}else {
-							long key = convert(lineQ);
-							String string = Long.toString(key);
-							if(lineTmp.substring(0, i).contentEquals(string)) {
-								found = true;
-								cache.addToCache(lineTmp.substring(0, i),lineQ);
-								pw.println(lineQ.toLowerCase() + lineTmp.substring(i, lineTmp.length()));
-								
-							}
+				Long keyQ;
+				if(useCache) {
+
+					if((keyQ=((Long)(cache.search(lineQ)))) != null ) {
+						String results = tree.readFileSearch(RAF, keyQ);
+						if (!results.contentEquals("none")) {
+							pw.println(results);
 						}
 					}
-				}else {
-					long key = convert(lineQ);
-					String string = Long.toString(key);
-					while(scanTmp.hasNextLine()) {
-						lineTmp=scanTmp.nextLine();
-						int i = 0;
-						while(lineTmp.charAt(i) != ':') {
-							i++;
-						}
-						if(lineTmp.substring(0, i).contentEquals(string)) {
-							pw.println(lineQ.toLowerCase() + lineTmp.subSequence(i, lineTmp.length()));
-						}
+					else {
+						keyQ = convert(lineQ);
+						cache.addToCache(lineQ, keyQ);
+						String results = tree.readFileSearch(RAF, keyQ);
+						if (!results.contentEquals("none")) {
+							pw.println(results);
+						}						
 					}
-					scanTmp.close();
+				}
+				else {
+					keyQ = convert(lineQ);
+					String results = tree.readFileSearch(RAF, keyQ);
+					if (!results.contentEquals("none")) {
+						pw.println(results);
+					}				
 				}
 			}
 
-
 			scanQ.close();
 			pw.close();
-
 
 
 		}catch(Exception e) {
@@ -126,17 +105,27 @@ public class GeneBankSearch {
 		if(debugLevel == 0) {
 			System.out.println(" print the query output to STDOUT");
 		}
-		
+		long timenow;
+		timenow = System.currentTimeMillis();
+		System.out.println("Time to run: " + (timenow-time));
 	}
 
+	/**
+	 * print usage
+	 */
 	public static void printUsage() {
 		System.out.println("java GeneBankSearch <0/1(no/with Cache)> <btree file> <query file> [<cache size>] [<debug level>]");
 	}
+
+	/**
+	 * converts a string of genes to a binary long
+	 * @param character
+	 * @return
+	 */
 	public static long convert(String character) {
 
 		String s = "";
 		for(int j = 0; j < character.length(); j++) {
-
 
 			if(character.charAt(j) == 'a' || character.charAt(j) == 'A') {
 				s += "00";
@@ -152,8 +141,6 @@ public class GeneBankSearch {
 			}
 			if(character.charAt(j) == 'n' || character.charAt(j) == 'N') {
 				s += "n";
-
-
 			}
 		}
 
